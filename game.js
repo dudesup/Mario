19729e3fc3490c8dbafe063d38d5d5bba8d3d529
Daria Supińska <dariasupinska@gmail.com>
2022-01-16 21:00:07 +0100

    Mario can go to the next level via pipe

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ kaboom({
   let CURRENT_JUMP_FORCE = JUMP_FORCE
   const BIG_JUMP_FORCE = 550
   let isJumping = true
+  const FALL_DEATH = 400
 
 loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
@@ -26,10 +27,17 @@ loadSprite('pipe-top-right', 'hj2GK4n.png')
 loadSprite('pipe-bottom-left', 'c1cYSbt.png')
 loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
-scene("game", ({score}) => {
+loadSprite('blue-block', 'fVscIbn.png')
+loadSprite('blue-brick', '3e5YRQd.png')
+loadSprite('blue-steel', 'gqVoI2b.png')
+loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+loadSprite('blue-surprise', 'RMqCc1G.png')
+
+scene("game", ({level, score}) => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
-    const map = [
+    const maps = [
+        [
         '                                              ',
         '                                              ',
         '                                              ',
@@ -41,7 +49,22 @@ scene("game", ({score}) => {
         '                                -+            ',
         '                      ^       ^ ()            ',
         '===================================    =======',
+    ],
+    [
+        '£                                           £',
+        '£                                           £',
+        '£                                           £',
+        '£                                           £',
+        '£                                           £',
+        '£        @@@@@@              x x            £',
+        '£                          x x x            £',
+        '£                        x x x x  x       -+£',
+        '£               z   z  x x x x x  x       ()£',
+        '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
     ]
+
+
+]
 
     const levelCfg = {
         width:20,
@@ -53,14 +76,19 @@ scene("game", ({score}) => {
         '}': [sprite('unboxed'), solid()],
         '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
         ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
-        '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
-        '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
+        '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
+        '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
         '^': [sprite('evil-shroom'), solid(), 'dangerous'],
         '#': [sprite('mushroom'), solid(), 'mushroom', body()],
+        '!': [sprite('blue-block'), solid(), scale(0.5)],
+        '£': [sprite('blue-brick'), solid(), scale(0.5)],
+        'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+        '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+        'x': [sprite('blue-steel'), solid(), scale(0.5)],
 
     }
 
-    const gameLevel = addLevel(map, levelCfg)
+    const gameLevel = addLevel(maps[level], levelCfg)
 
     const scoreLabel = add([
         text(score),
@@ -71,7 +99,7 @@ scene("game", ({score}) => {
         }
     ])
 
-    add([text('level ' + score, pos(4,6))])
+    add([text('level ' + parseInt(level + 1) ), pos(40, 6)])
 
     function big() {
         let timer = 0
@@ -154,6 +182,25 @@ scene("game", ({score}) => {
         }
     })
 
+    player.action(() => {
+        camPos(player.pos)
+        if (player.pos.y >= FALL_DEATH) {
+          go('lose', {
+              score: scoreLabel.value
+            })
+        }
+      })
+
+
+      player.collides('pipe', () => {
+        keyPress('down', () => {
+          go('game', {
+            level: (level + 1) % maps.length,
+            score: scoreLabel.value
+          })
+        })
+      })
+
     keyDown('left', ()=> {
         player.move(-MOVE_SPEED, 0)
     })
@@ -179,4 +226,4 @@ scene("game", ({score}) => {
 scene('lose', ({score}) => {
     add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
 })
-start("game", {score: 0})
+start("game", {level: 0, score: 0})
